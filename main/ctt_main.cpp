31f9d0fb0471a8565024c511b61b83386bb55b96
wu6u3 <tinyinlai@gmail.com>
2017-05-30 17:31:46 -0500

    modified:   ../main/.ctt_main.cpp.swp 	modified:   ../main/ctt_main.cpp 	modified:   ctt_utility.hpp

--- main/ctt_main.cpp
+++ main/ctt_main.cpp
@@ -11,4 +11,14 @@ int main(){
   std::cout<<sum(1.3,2.3,3.6,4.6,56.4,7623.4,7.2)<<std::endl;
   //std::cout<<su<<std::endl;
   std::cout<<multi(23.45,3434.234,34325.2343,23523526.3)<<std::endl;
+  std::cout<<interpolate1D(1.0, 2.0, 0.2)<<std::endl;
+  std::cout<<interpolate1D(10.0, 20.0,0.2)<<std::endl;
+  std::cout<<interpolate1D(20.0, 30.0,0.2)<<std::endl;
+  std::cout<<interpolate1D(interpolate1D(10.0, 20.0,0.2), interpolate1D(20.0, 30.0,0.2), 0.3)<<std::endl;
+  std::cout<<interpolate2D(10.0, 20.0, 20.0, 30.0, 0.2, 0.3)<<std::endl;
+  static_assert(sum(1.3,2.3,3.6,4.6,56.4,7623.4,7.2));
+  static_assert(multi(23.45,3434.234,34325.2343));
+  static_assert(interpolate1D(1.0, 2.0, 0.2));
+  static_assert(interpolate2D(10.0, 20.0, 20.0, 30.0, 0.2, 0.3));
+  
 }
--- src/ctt_utility.hpp
+++ src/ctt_utility.hpp
@@ -15,49 +15,54 @@ static constexpr auto multi(T... s){
   return (...*s);
 }
 
+
+
 /*  
 
-a1--o------a2
+interpolate1D
+Given 2 values
+a0 a1 and ratio p 
+return a 1-D interpolation value
 
-|a1o|/|a1a2| = p
+a0--o------a1
+
+|a0o|/|a0a1| = p
 
 */
 
 template <typename T>
-static constexpr auto interpolate1D(T a1, T a2, const double p){
-  if constexpr (a2 > a1){
-    return ( a2 - a1 ) * p + a2;
-  }
-  else {
-    return ( a1 - a2 ) * p + a2; 
-  }
+static constexpr auto interpolate1D(T a0, T a1, const double p){
+  return (a1 > a0) ? (( a1 - a0 ) * p + a0) : (( a0 - a1 ) * p + a1);
 }
 
+
+
+
 /*  
+interpolate2D
+Given 2x2 values a00 a01 a10 a11 and partition p1 and p2
+return a 2-D interpolation value
+ 
+    p1
+  a00--oa------a01
+p2|             |
+  ob    *       |
+  |             |
+  a10----------a11
 
-  a1--oa------a2 
-b1
-|
-ob    *
-|
-b2
 
-
-|a1oa|/|a2a1| = pa
-
-
+|a00 oa|/|a00a01| = p1
 
 */
-
-
 template <typename T>
-static constexpr auto interpolate2D(T a1, T a2, T b1, T b2, const double pa, const double pb){
-
-  //constexpr auto ao1 =     
+static constexpr auto interpolate2D(T a00, T a01, T a10, T a11, const double p1, const double p2){
+  return interpolate1D(interpolate1D(a00, a01,p1), interpolate1D(a10, a11,p1), p2);
   
 
 
 }
+
+
 
 
 template<typename... T>
